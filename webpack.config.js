a1f47c5b21fdf346501b55b636295cc4f38a88e1
hendridg <hendridg@gmail.com>
2022-01-12 00:13:48 -0400

    Change webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,6 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   entry: {
     index: './src/index.js',
-    task: './src/task.js',
   },
   plugins: [
     new HtmlWebpackPlugin({
